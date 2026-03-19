2059910342687db8b361630c72d0e3c615609768
Didier Pereira <krrattoss5@gmail.com>
2026-03-18 20:55:38 -0500

    fix(models): remove duplicate declarations in ShoppingCart shim

--- backend/src/models/mongoose/ShoppingCart.js
+++ backend/src/models/mongoose/ShoppingCart.js
@@ -18,39 +18,3 @@ const cartSchema = new Schema(
 );
 
 module.exports = models.ShoppingCart || model('ShoppingCart', cartSchema);
-const mongoose = require('mongoose');
-
-const cartItemSchema = new mongoose.Schema({
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
-    required: true
-  },
-  quantity: {
-    type: Number,
-    default: 1,
-    min: 1
-  },
-  price: {
-    type: Number,
-    min: 0
-  }
-}, { _id: false });
-
-const shoppingCartSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    unique: true
-  },
-  items: [cartItemSchema],
-  active: {
-    type: Boolean,
-    default: true
-  }
-}, {
-  timestamps: true
-});
-
-module.exports = mongoose.model('ShoppingCart', shoppingCartSchema);
